f34b5e0c504bd279d2994a9d288fea615524bdb6
Tasnimul Haque <tasnimul.haque6@gmail.com>
2023-11-26 22:46:22 +0600

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <>
       <Header></Header>
       <Outlet></Outlet>
-      
+      <Footer></Footer>
     </>
   )
 }
