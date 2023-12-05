f12a63e533132f1bc1823c1692b50baa18a6f5a6
Tasnimul Haque <tasnimul.haque6@gmail.com>
2023-12-05 08:25:09 +0600

    dummy portfolio added

--- src/Components/PortfolioCard/PortfolioCard.jsx
+++ src/Components/PortfolioCard/PortfolioCard.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
 
-const PortfolioCard = () => {
+const PortfolioCard = ({portfolio}) => {
     return (
-        <div className='bg-white'>
-            <div>
-                
+        <div className='bg-white border p-4 rounded-lg group hover:border-themeColor hover:cursor-default overflow-hidden'>
+            <div className="overflow-hidden rounded-lg">
+                <img className="rounded-lg group-hover:scale-125 duration-300" src={portfolio.portfolioThumbnail} alt="Portfolio Thumnail" />
+            </div>
+            <div className="mt-5 space-y-2">
+                <h2 className="font-semibold text-xl">{portfolio.portfolioTitle}</h2>
+                <p className="text-base">{ portfolio.shortDescription}</p>
             </div>
         </div>
     );
--- src/Pages/PPortfolios/PPortfolios.jsx
+++ src/Pages/PPortfolios/PPortfolios.jsx
@@ -1,20 +1,31 @@
 import { useLoaderData } from "react-router-dom";
+import PortfolioCard from "../../Components/PortfolioCard/PortfolioCard";
 
+const portfolios = [
+    {"portfolioTitle": "Dhaka Startup", "shortDescription": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repellendus tempora assumenda, harum atque perferendis. Totam tenetur corporis accusantium at.", "portfolioThumbnail": "https://images.unsplash.com/photo-1682687220198-88e9bdea9931?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "portfolioLink": "#"},
+    {"portfolioTitle": "Dhaka Startup", "shortDescription": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repellendus tempora assumenda, harum atque perferendis. Totam tenetur corporis accusantium at.", "portfolioThumbnail": "https://images.unsplash.com/photo-1682687220198-88e9bdea9931?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "portfolioLink": "#"},
+    {"portfolioTitle": "Dhaka Startup", "shortDescription": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repellendus tempora assumenda, harum atque perferendis. Totam tenetur corporis accusantium at.", "portfolioThumbnail": "https://images.unsplash.com/photo-1682687220198-88e9bdea9931?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "portfolioLink": "#"},
+    {"portfolioTitle": "Dhaka Startup", "shortDescription": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repellendus tempora assumenda, harum atque perferendis. Totam tenetur corporis accusantium at.", "portfolioThumbnail": "https://images.unsplash.com/photo-1682687220198-88e9bdea9931?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "portfolioLink": "#"},
+    {"portfolioTitle": "Dhaka Startup", "shortDescription": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit repellendus tempora assumenda, harum atque perferendis. Totam tenetur corporis accusantium at.", "portfolioThumbnail": "https://images.unsplash.com/photo-1682687220198-88e9bdea9931?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", "portfolioLink": "#"},
+]
 
 const PPortfolios = () => {
     const loadedPortfolios = useLoaderData()
     return (
-        <div className="bg-white py-28">
+        <div className="bg-white py-24">
             <div className="w-11/12 m-auto">
-                <h1>This is portfolio page</h1>
-                <p>portfolio length {loadedPortfolios.length}</p>
                 {/* Portfolio Card */}
-                <div>
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-7">
+                    {
+                        portfolios.map(portfolio => <PortfolioCard portfolio={portfolio} key={portfolio.portfolioTitle}>
 
+                        </PortfolioCard>)
+                    }
                 </div>
             </div>
         </div>
     );
 };
+
 
 export default PPortfolios;
