896281ffd2d1ed2e1d0f6cb136c44956ffb3ff4a
Tasnimul Haque <tasnimul.haque6@gmail.com>
2023-12-10 02:35:21 +0600

    added services reviews

--- src/Pages/Services/GraphicsDesign/GraphicsDesign.jsx
+++ src/Pages/Services/GraphicsDesign/GraphicsDesign.jsx
@@ -1,4 +1,7 @@
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from "swiper/modules";
 
 
 const keys = [
@@ -17,6 +20,13 @@ const keys = [
 
 
 const GraphicsDesign = () => {
+            const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('https://cubitose-backend-production.up.railway.app/review/graphics-design')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+        
+    },[])
     return (
         <div>
             <div className="bg-white py-28">
@@ -75,7 +85,36 @@ const GraphicsDesign = () => {
 
                     </div>
                 </div>
+                {/* Reviews Section */}
+                <div className='mt-28'>
+                            <Swiper
+                            modules={[Autoplay,]}
+                            autoplay={{
+                            delay: 3000,
+                            disableOnInteraction: false,
+                            }}>
+                            {reviews.map(review => <SwiperSlide key={review._id}>
+                                    
+                            <figure className="max-w-screen-md mx-auto text-center">
+                                <svg className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                                    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+                                </svg>
+                                <blockquote>
+                                    <p className="text-base lg:text-xl italic text-gray-900 dark:text-white">&quot;{review.customerReview}&quot;</p>
+                                </blockquote>
+                                <figcaption className="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
+                                    
+                                    <div className="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700">
+                                        <cite className="pe-3 font-medium text-gray-900 dark:text-white">{ review.customerName}</cite>
+                                        <cite className="ps-3 text-sm text-gray-500 dark:text-gray-400">{ review.customerPosition}</cite>
+                                    </div>
+                                </figcaption>
+                            </figure>
 
+                        </SwiperSlide>)}
+                            </Swiper>
+                        
+                    </div>
             </div>
         </div>
     );
