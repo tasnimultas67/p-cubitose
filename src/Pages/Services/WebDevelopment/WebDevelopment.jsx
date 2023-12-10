bbc4dd972cad0d7578fdb16e70ba43df79baaa80
Tasnimul Haque <tasnimul.haque6@gmail.com>
2023-12-10 16:04:19 +0600

    added animation to web dev service

--- src/Pages/Services/WebDevelopment/WebDevelopment.jsx
+++ src/Pages/Services/WebDevelopment/WebDevelopment.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from "swiper/modules";
 import { useLoaderData } from 'react-router-dom';
+import {  motion } from 'framer-motion'
 
 
 const processes = [
@@ -31,45 +32,101 @@ const WebDevelopment = () => {
         <div className="bg-white py-28">
             <div className="w-11/12 m-auto">
                 <div>
-                    <h1 className="text-3xl lg:text-5xl text-black lg:leading-tight">A solid, well-planned and well-executed website can set you apart from your competition, drive bottom line value, and make you the star of the show</h1>
+                    <motion.h1
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 0.5,
+                        }}
+                        viewport={{ once: true }}
+                        className="text-3xl lg:text-5xl text-black lg:leading-tight">A solid, well-planned and well-executed website can set you apart from your competition, drive bottom line value, and make you the star of the show</motion.h1>
                 </div>
                 <div className="grid grid-cols-1 lg:grid-cols-4 mt-10 lg:mt-20">
                     <div className='hidden lg:block'></div>
-                    <div className="col-span-3 flex flex-col gap-2">
+                    <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 0.8,
+                        }}
+                        viewport={{ once: true }}
+                        className="col-span-3 flex flex-col gap-2">
                         <p>Your website is the heart of your digital identity: it should be dynamic, interactive and bring visitors back to engage with your brand.</p>
                         <p>At Cubitose, we pride ourselves on building bespoke websites that weave your business needs into the back-end, enabling your teams to manage your site and create engaging content.</p>
                         <p>Guided by your vision, our development team will develop the custom features you need to achieve your goals.</p>
                         <p>Using the latest front-end technologies, we’ll create subtle animations and interactive elements to wow your audience without compromising on functionality.</p>
                         <p>The result is a custom-built website that’s fast, reliable, secure and ready to scale with your business.</p>
-                    </div>
+                    </motion.div>
 
                 </div>
                 {/* Banner Image */}
-                <div className="mt-10 lg:mt-20">
+                <motion.div
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{opacity: 1, y: 0}}
+                    transition={{
+                        duration: 1,
+                    }}
+                    viewport={{ once: true }}
+                    className="mt-10 lg:mt-20">
                     <img loading='lazy'
                         className="h-[20rem] lg:h-[30rem] 2xl:h-[35rem] w-full object-cover object-bottom rounded-lg shadow-lg"
                         src="https://i.ibb.co/HptGPFn/Banner-1.webp" alt="Web Development Banner" srcSet=""/>
-                </div>
+                </motion.div>
                 {/* Feature process*/}
                 <div className="pt-20 lg:pt-36 pb-10 lg:pb-20">
-                    <h2 className="text-3xl lg:text-5xl font-medium text-black">How come we are so proficient at this</h2>
+                    <motion.h2
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 0.5,
+                        }}
+                        viewport={{ once: true }}
+                        className="text-3xl lg:text-5xl font-medium text-black">How come we are so proficient at this</motion.h2>
                     <div className="grid grid-cols-1 lg:grid-cols-4 gap-10 mt-16 lg:mt-28 mb-10">
-                        <div>
+                        <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 0.8,
+                        }}
+                        viewport={{ once: true }}
+                                    >
                             <h2 className="text-2xl font-semibold text-black mb-3 flex items-center uppercase">Reliable <ArrowUpRightIcon className='h-5 w-5 ml-2'></ArrowUpRightIcon></h2>
                             <p>Cubitose develops websites with custom functionality to meet your needs. We take pride in coding back-end solutions that act as the unseen backbone to your website.</p>
-                        </div>
-                        <div>
+                        </motion.div>
+                        <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 1,
+                        }}
+                        viewport={{ once: true }}
+                                    >
                             <h2 className="text-2xl font-semibold text-black mb-3 flex items-center uppercase">Lightweight <ArrowUpRightIcon className='h-5 w-5 ml-2'></ArrowUpRightIcon></h2>
                             <p>We make use of the latest technologies and rely on high-speed HTML, JavaScript and CSS to deliver best-practice website code, often using a headless approach to the CMS.</p>
-                        </div>
-                        <div>
+                        </motion.div>
+                        <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 1.5,
+                        }}
+                        viewport={{ once: true }}
+                                    >
                             <h2 className="text-2xl font-semibold text-black mb-3 flex items-center uppercase">Responsive <ArrowUpRightIcon className='h-5 w-5 ml-2'></ArrowUpRightIcon></h2>
                             <p>We test, tweak and optimise websites to ensure the most seamless experience, regardless of the user’s device.</p>
-                        </div>
-                        <div>
+                        </motion.div>
+                        <motion.div
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 2,
+                        }}
+                        viewport={{ once: true }}
+                                    >
                             <h2 className="text-2xl font-semibold text-black mb-3 flex items-center uppercase">Optimised <ArrowUpRightIcon className='h-5 w-5 ml-2'></ArrowUpRightIcon></h2>
                             <p>Our team’s technical expertise allows us to deliver high-performing websites that are fully optimised in line with SEO best practice.</p>
-                        </div>
+                        </motion.div>
                         
                     </div>
                 </div>
@@ -78,14 +135,28 @@ const WebDevelopment = () => {
             {/* Development Process */}
                 <div className="bg-themeColor lg:py-28 py-24">
                     <div className='w-11/12 m-auto'>
-                        <h1 className="text-3xl lg:text-5xl text-white">Our Development Process</h1>
+                    <motion.h1
+                        initial={{ opacity: 0, y: 100 }}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            duration: 0.5,
+                        }}
+                        viewport={{ once: true }}
+                        className="text-3xl lg:text-5xl text-white">Our Development Process</motion.h1>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-20 gap-10">
                             {
                                 processes.map(process => (
-                                    <div key={process.id}>
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 100 }}
+                                        whileInView={{opacity: 1, y: 0}}
+                                        transition={{
+                                            duration: 0.8,
+                                        }}
+                                        viewport={{ once: true }}
+                                        key={process.id}>
                                         <h2 className="mb-5 text-xl text-white font-semibold uppercase flex items-center">{ process.title}<ArrowUpRightIcon className='h-5 w-5 ml-2'></ArrowUpRightIcon></h2>
                                         <p className='text-white'>{ process.description}</p>
-                                    </div>
+                                    </motion.div>
                                 ))
                             }
                             
@@ -94,7 +165,14 @@ const WebDevelopment = () => {
                     </div>
             </div>
             {/* Reviews Section */}
-            <div className='mt-28'>
+            <motion.div
+                initial={{ opacity: 0, y: 100 }}
+              whileInView={{opacity: 1, y: 0}}
+              transition={{
+                  duration: 0.5,
+              }}
+              viewport={{ once: true }}
+                className='mt-28'>
                     <Swiper
                     modules={[Autoplay,]}
                     autoplay={{
@@ -122,7 +200,7 @@ const WebDevelopment = () => {
         </SwiperSlide>)}
                     </Swiper>
                 
-            </div>
+            </motion.div>
         </div>
     );
 };
