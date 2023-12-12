350575c8f3580b306e259cf826a38ae7a345b25f
Tasnimul Haque <tasnimul.haque6@gmail.com>
2023-12-12 15:53:26 +0600

    marquee updated

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,32 @@ function App() {
     }
    }, [pageLocation.pathname])
   
+  useEffect(() => {
+        const handleContextmenu = e => {
+            e.preventDefault()
+        }
+        document.addEventListener('contextmenu', handleContextmenu)
+        return function cleanup() {
+          document.removeEventListener('contextmenu', handleContextmenu)
+
+          
+          document.onkeydown = function (e) {
+          if (event.keyCode == 123) {
+              return false;
+          }
+          if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+              return false;
+          }
+          if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+              return false;
+          }
+          if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+              return false;
+          }
+      }
+        }
+    
+}, [ ])
   return (
     <>
       <ScrollRestoration
